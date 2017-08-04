375273d71fa1a39fa4b767ea140d8c4bbee8187d
mauro <mauro@crs4.it>
2017-08-04 16:46:16 +0200

    removed creation of META Observation

--- src/main/java/org/bch/fhir/i2b2/service/ObservationToI2B2.java
+++ src/main/java/org/bch/fhir/i2b2/service/ObservationToI2B2.java
@@ -61,7 +61,7 @@ public class ObservationToI2B2 extends FHIRToPDO {
             pdo.addElementSet(observationSet);
 
             // We add metadata for admin purposes
-            addMetadataInObservationSet("Observation", METADATA_CONCEPT_CD, observationSet);
+//            addMetadataInObservationSet("Observation", METADATA_CONCEPT_CD, observationSet);
         }
         return pdo.generatePDOXML();
     }
