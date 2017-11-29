204f30693c95f077062b8cd45ed355e101231302
mauro <mauro@crs4.it>
2017-11-29 15:55:22 +0100

    EncounterToI2B2 basic version.

---
+++ src/main/java/org/bch/fhir/i2b2/service/EncounterToI2B2.java
@@ -0,0 +1,31 @@
+package org.bch.fhir.i2b2.service;
+
+import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
+import ca.uhn.fhir.model.dstu2.resource.BaseResource;
+import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import org.bch.fhir.i2b2.exception.FHIRI2B2Exception;
+import org.bch.fhir.i2b2.pdomodel.PDOModel;
+
+/**
+ * Created by mauro on 29/11/17.
+ */
+public class EncounterToI2B2 extends FHIRToPDO  {
+
+    @Override
+    public String getPDOXML(BaseResource resource) throws FHIRI2B2Exception {
+        Encounter enc = (Encounter) resource;
+
+        this.eventIde = this.getEventId(enc);
+        this.patientIde = getPatientId(enc);
+
+        PDOModel pdo = new PDOModel();
+        pdo.addElementSet(generateEventSet(enc));
+        return pdo.generatePDOXML();
+    }
+
+    protected  String getPatientId(Encounter enc) throws FHIRI2B2Exception {
+        ResourceReferenceDt refPatient = enc.getPatient();
+        if (refPatient.isEmpty()) throw new FHIRI2B2Exception("Subject reference is not informed");
+        return refPatient.getReference().getIdPart();
+    }
+}
---
+++ src/test/java/org/bch/fhir/i2b2/service/EncounterToI2B2Test.java
@@ -0,0 +1,77 @@
+package org.bch.fhir.i2b2.service;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import ca.uhn.fhir.parser.IParser;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by mauro on 29/11/17.
+ */
+public class EncounterToI2B2Test extends BaseTest {
+    protected FhirContext ctx = FhirContext.forDstu2();
+
+    private  EncounterToI2B2 parseFile(String fileName) throws Exception {
+        IParser parser = ctx.newJsonParser();
+        InputStream in = DiagnosticReportToI2B2Test.class.getResourceAsStream(fileName);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        return (EncounterToI2B2) parser.parseResource((Class)EncounterToI2B2.class, br);
+
+    }
+
+    @Test
+    public void basicTest() throws Exception {
+        Encounter enc = (Encounter) parseFile(Encounter.class, "Encounter.json");
+        EncounterToI2B2 encounterToI2B2 = new EncounterToI2B2();
+        String xml = encounterToI2B2.getPDOXML(enc);
+
+        System.out.println(xml);
+        Document doc = parseXMLString(xml);
+        XPath xpath = XPathFactory.newInstance().newXPath();
+
+
+        NodeList eventList = (NodeList) xpath.evaluate("/patient_data/event_set/event", doc, XPathConstants.NODESET);
+        assertEquals(eventList.getLength(), 1);
+        Node eventId = (Node) xpath.evaluate(
+                String.format("/patient_data/event_set/event/event_id[text()='%s']", enc.getId().getIdPart()),
+                doc,
+                XPathConstants.NODE
+        );
+        assertNotNull(eventId);
+        Node patientId = (Node) xpath.evaluate(
+                String.format("/patient_data/event_set/event/patient_id[text()='%s']", enc.getPatient().getReference().getIdPart()),
+                doc,
+                XPathConstants.NODE
+        );
+        assertNotNull(patientId);
+
+        Node startDate = (Node) xpath.evaluate(
+                String.format("/patient_data/event_set/event/start_date[text()='%s']", "1850-06-11T00:00:00.00"),
+                doc,
+                XPathConstants.NODE
+        );
+        assertNotNull(startDate);
+
+        Node endDate = (Node) xpath.evaluate(
+                String.format("/patient_data/event_set/event/end_date[text()='%s']", "1850-06-19T00:00:00.00"),
+                doc,
+                XPathConstants.NODE
+        );
+        assertNotNull(endDate);
+
+
+    }
+}
