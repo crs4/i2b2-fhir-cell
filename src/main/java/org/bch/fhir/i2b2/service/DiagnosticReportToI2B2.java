91641ae928f7a479214db66076d6f20ac5fc4e5a
mauro <mauro@crs4.it>
2017-12-05 16:46:30 +0100

    new method for pdo generation

--- src/main/java/org/bch/fhir/i2b2/service/DiagnosticReportToI2B2.java
+++ src/main/java/org/bch/fhir/i2b2/service/DiagnosticReportToI2B2.java
@@ -1,14 +1,8 @@
 package org.bch.fhir.i2b2.service;
 
 import ca.uhn.fhir.model.api.IResource;
-import ca.uhn.fhir.model.dstu2.composite.CodingDt;
-import ca.uhn.fhir.model.dstu2.composite.ContainedDt;
-import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
-import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
-import ca.uhn.fhir.model.dstu2.resource.BaseResource;
-import ca.uhn.fhir.model.dstu2.resource.DiagnosticReport;
-import ca.uhn.fhir.model.dstu2.resource.Encounter;
-import ca.uhn.fhir.model.dstu2.resource.Observation;
+import ca.uhn.fhir.model.dstu2.composite.*;
+import ca.uhn.fhir.model.dstu2.resource.*;
 import ca.uhn.fhir.model.primitive.DateTimeDt;
 import org.apache.axis2.databinding.types.soapencoding.DateTime;
 import org.apache.commons.lang3.StringUtils;
@@ -36,73 +30,156 @@ public class DiagnosticReportToI2B2 extends FHIRToPDO {
         return getPDO(resource).generatePDOXML();
     }
 
+
+
     @Override
     protected PDOModel getPDO(BaseResource resource) throws FHIRI2B2Exception {
         this.report = (DiagnosticReport) resource;
-        Observation obs = new Observation();
-        Encounter enc = new Encounter();
-        enc.setId(report.getEncounter().getReference().getIdPart());
+        PDOModel pdoModel = new PDOModel();
 
-        PeriodDt period;
-        try {
-            period = (PeriodDt) report.getEffective();
+        this.patientIde = report.getSubject().getReference().getIdPart();
+        Patient patient = (Patient) findResourceById(report.getContained().getContainedResources(), this.patientIde);
+        if (patient != null) {
+//            this.patientIdeSource = patient.getManagingOrganization().getReference().getIdPart();
+            PatientToI2B2 patientToI2B2 = new PatientToI2B2();
+            patientToI2B2.patientIde = this.patientIde;
+            patientToI2B2.patientIdeSource = this.patientIdeSource;
+            patientToI2B2.eventIdeSource = this.eventIdeSource;
+            pdoModel.addElementSet(patientToI2B2.generatePatientSet(patient));
+        }
+        else {
+            pdoModel.addElementSet(generatePatientSet());
+        }
+
+        this.eventIde = report.getEncounter().getReference().getIdPart();
+        Encounter enc = (Encounter) findResourceById(report.getContained().getContainedResources(), this.eventIde);
+        if (enc != null) {
+//            this.eventIdeSource = enc.getServiceProvider().getReference().getIdPart();
 
         }
-        catch (ClassCastException castException) {
-            period = new PeriodDt();
-            DateTimeDt datetime = (DateTimeDt) report.getEffective();
-            period.setStart(datetime);
-            period.setEnd(datetime);
-
-        }
-        enc.setPeriod(period);
-        obs.setEffective(period.getStartElement());
-
-        obs.setEncounter(report.getEncounter());
-        obs.setSubject(report.getSubject());
-        if (!report.getCodedDiagnosis().isEmpty()) {
-            obs.setCode(report.getCodedDiagnosis().get(0));
-            obs.setValue(report.getCodedDiagnosis().get(0).getCoding().get(0).getDisplayElement());
-        }
-//        obs.setEffective(report.getEffective());
-        List<ResourceReferenceDt> performerList = new ArrayList<>();
-        performerList.add(report.getPerformer());
-        obs.setPerformer(performerList);
-        ContainedDt contained = new ContainedDt();
-        List<IResource> containedList = report.getContained().getContainedResources();
-        containedList.add(enc);
-        contained.setContainedResources(containedList);
-        obs.setContained(contained);
-        PDOModel pdoModel = observationToI2B2.getPDO(obs);
-
-
-        System.out.println("this.report.getContained().getContainedResources().size() " + this.report.getContained().getContainedResources().size());
-        for (IResource ir : this.report.getContained().getContainedResources()){
-            if (ir instanceof Observation) {
-                PDOModel containedPDO = observationToI2B2.getPDO((BaseResource) ir);
-
-                String [] pdo_set_array = new String[]{
-                        ElementSet.PDO_OBSERVATION_SET,
-                        ElementSet.PDO_EVENT_SET,
-                        ElementSet.PDO_EID_SET,
-//                    ElementSet.PDO_PATIENT_SET,
-                        ElementSet.PDO_PID_SET,
-                        ElementSet.PDO_CONCEPT_SET,
-                };
-
-                for (String pdo_set: pdo_set_array) {
-                    for(Element el: containedPDO.getElementSet(pdo_set).getElements()) {
-                        pdoModel.getElementSet(pdo_set).addElement(el);
-                    }
-                }
-            }
+        else {
+            enc = new Encounter();
+            enc.setId(this.eventIde);
         }
 
 
-//        this.eventIde = this.getEventId(enc);
-//        this.patientIde = this.eventIde;
-//        pdoModel.addElementSet(generateEventSet(enc));
+        pdoModel.addElementSet(generateEIDSet());
+        pdoModel.addElementSet(generatePIDSet());
+        pdoModel.addElementSet(generateEventSet(enc));
+        pdoModel.addElementSet(generateObservationSet(report));
+
+
+
+
+
+
+//        Observation obs = new Observation();
+//        Encounter enc = new Encounter();
+//        enc.setId(report.getEncounter().getReference().getIdPart());
+//
+//        PeriodDt period;
+//        try {
+//            period = (PeriodDt) report.getEffective();
+//
+//        }
+//        catch (ClassCastException castException) {
+//            period = new PeriodDt();
+//            DateTimeDt datetime = (DateTimeDt) report.getEffective();
+//            period.setStart(datetime);
+//            period.setEnd(datetime);
+//
+//        }
+//        enc.setPeriod(period);
+//        obs.setEffective(period.getStartElement());
+//
+//        obs.setEncounter(report.getEncounter());
+//        obs.setSubject(report.getSubject());
+//        if (!report.getCodedDiagnosis().isEmpty()) {
+//            obs.setCode(report.getCodedDiagnosis().get(0));
+//            obs.setValue(report.getCodedDiagnosis().get(0).getCoding().get(0).getDisplayElement());
+//        }
+////        obs.setEffective(report.getEffective());
+//        List<ResourceReferenceDt> performerList = new ArrayList<>();
+//        performerList.add(report.getPerformer());
+//        obs.setPerformer(performerList);
+//        ContainedDt contained = new ContainedDt();
+//        List<IResource> containedList = report.getContained().getContainedResources();
+//        containedList.add(enc);
+//        contained.setContainedResources(containedList);
+//        obs.setContained(contained);
+//        PDOModel pdoModel = observationToI2B2.getPDO(obs);
+//
+//
+//        System.out.println("this.report.getContained().getContainedResources().size() " + this.report.getContained().getContainedResources().size());
+//        for (IResource ir : this.report.getContained().getContainedResources()){
+//            if (ir instanceof Observation) {
+//                PDOModel containedPDO = observationToI2B2.getPDO((BaseResource) ir);
+//
+//                String [] pdo_set_array = new String[]{
+//                        ElementSet.PDO_OBSERVATION_SET,
+//                        ElementSet.PDO_EVENT_SET,
+//                        ElementSet.PDO_EID_SET,
+////                    ElementSet.PDO_PATIENT_SET,
+//                        ElementSet.PDO_PID_SET,
+//                        ElementSet.PDO_CONCEPT_SET,
+//                };
+//
+//                for (String pdo_set: pdo_set_array) {
+//                    for(Element el: containedPDO.getElementSet(pdo_set).getElements()) {
+//                        pdoModel.getElementSet(pdo_set).addElement(el);
+//                    }
+//                }
+//            }
+//        }
+//
+//
+////        this.eventIde = this.getEventId(enc);
+////        this.patientIde = this.eventIde;
+////        pdoModel.addElementSet(generateEventSet(enc));
 
         return pdoModel;
+    }
+
+    protected ElementSet generateObservationSet(DiagnosticReport report) throws FHIRI2B2Exception {
+        ElementSet observationSet = new ElementSet();
+        observationSet.setTypePDOSet(ElementSet.PDO_OBSERVATION_SET);
+        Observation obs;
+
+        ObservationToI2B2 observationToI2B2 = new ObservationToI2B2();
+        observationToI2B2.patientIdeSource = this.patientIdeSource;
+        observationToI2B2.patientIde = this.patientIde;
+        observationToI2B2.eventIde = this.eventIde;
+        observationToI2B2.eventIde = this.eventIde;
+
+        for (CodeableConceptDt diagnosis: report.getCodedDiagnosis()) {
+            obs = new Observation();
+            PeriodDt period;
+            try {
+                period = (PeriodDt) report.getEffective();
+
+            }
+            catch (ClassCastException castException) {
+                period = new PeriodDt();
+                DateTimeDt datetime = (DateTimeDt) report.getEffective();
+                period.setStart(datetime);
+                period.setEnd(datetime);
+
+            }
+            obs.setEffective(period.getStartElement());
+            obs.setEncounter(report.getEncounter());
+            obs.setSubject(report.getSubject());
+
+            obs.setCode(diagnosis);
+            obs.setValue(diagnosis.getCoding().get(0).getDisplayElement());
+
+    //        obs.setEffective(report.getEffective());
+            List<ResourceReferenceDt> performerList = new ArrayList<>();
+            performerList.add(report.getPerformer());
+            obs.setPerformer(performerList);
+            ElementSet obsSet = observationToI2B2.generateObservationSet(obs);
+            observationSet.addElement(obsSet.getElements().get(0));
+
+        }
+        return observationSet;
     }
 }
--- src/test/java/org/bch/fhir/i2b2/service/DiagnosticReportToI2B2Test.java
+++ src/test/java/org/bch/fhir/i2b2/service/DiagnosticReportToI2B2Test.java
@@ -46,24 +46,21 @@ public class DiagnosticReportToI2B2Test extends BaseTest {
         assertEquals("7996923", patientID);
 
         NodeList observationList = doc.getElementsByTagName("observation");
-        Assert.assertTrue(observationList.getLength() == 2);
+        Assert.assertEquals(1, observationList.getLength());
 
-        NodeList conceptList = doc.getElementsByTagName("concept");
-        Assert.assertTrue(conceptList.getLength() == 2);
+//        NodeList conceptList = doc.getElementsByTagName("concept");
+//        Assert.assertEquals(1, conceptList.getLength());
 
 
-        Node conceptPath = (Node) xpath.evaluate("/patient_data/concept_set/*[1]/concept_path[text()='Sarcoma, NAS']", doc, XPathConstants.NODE);
-        assertNotNull(conceptPath);
-        Node conceptCD = (Node) xpath.evaluate("/patient_data/concept_set/concept/concept_cd[text()='Sarcoma, NAS']", doc, XPathConstants.NODE);
-        assertNotNull(conceptCD);
-        Node  nameChar = (Node) xpath.evaluate("/patient_data/concept_set/concept/name_char[text()='Sarcoma, NAS']", doc, XPathConstants.NODE);
-        assertNotNull(nameChar);
+//        Node conceptPath = (Node) xpath.evaluate("/patient_data/concept_set/*[1]/concept_path[text()='Sarcoma, NAS']", doc, XPathConstants.NODE);
+//        assertNotNull(conceptPath);
+//        Node conceptCD = (Node) xpath.evaluate("/patient_data/concept_set/concept/concept_cd[text()='Sarcoma, NAS']", doc, XPathConstants.NODE);
+//        assertNotNull(conceptCD);
+//        Node  nameChar = (Node) xpath.evaluate("/patient_data/concept_set/concept/name_char[text()='Sarcoma, NAS']", doc, XPathConstants.NODE);
+//        assertNotNull(nameChar);
 
         Node observationDiagnosisPatientID1 = (Node) xpath.evaluate("/patient_data/observation_set/*[1]/patient_id[text()='7996923']", doc, XPathConstants.NODE);
         assertNotNull(observationDiagnosisPatientID1);
-
-        Node observationDiagnosisPatientID2 = (Node) xpath.evaluate("/patient_data/observation_set/*[2]/patient_id[text()='7996923']", doc, XPathConstants.NODE);
-        assertNotNull(observationDiagnosisPatientID2);
 
         Node observationDiagnosisEventID1 = (Node) xpath.evaluate("/patient_data/observation_set/*[1]/event_id[text()='483572']", doc, XPathConstants.NODE);
         assertNotNull(observationDiagnosisEventID1);
@@ -71,9 +68,7 @@ public class DiagnosticReportToI2B2Test extends BaseTest {
         Node observationModifier1 = (Node) xpath.evaluate("/patient_data/observation_set/*[1]/modifier_cd[text()='@']", doc, XPathConstants.NODE);
         assertNotNull(observationModifier1);
 
-        Node observationModifier2 = (Node) xpath.evaluate("/patient_data/observation_set/*[2]/modifier_cd[text()='@']", doc, XPathConstants.NODE);
-        assertNotNull(observationModifier2);
-//
+
         Node observationDiagnosisConceptCD1 = (Node) xpath.evaluate("/patient_data/observation_set/*[1]/concept_cd[text()='Sarcoma, NAS']", doc, XPathConstants.NODE);
         assertNotNull(observationDiagnosisConceptCD1);
 //
@@ -81,64 +76,7 @@ public class DiagnosticReportToI2B2Test extends BaseTest {
                 "/patient_data/observation_set/*[1]/start_date[text()='1850-06-12T00:00:00.00']", doc, XPathConstants.NODE);
         assertNotNull(observationDiagnosisStartDate1);
 //
-//        Node observationDiagnosisEndDate1 = (Node) xpath.evaluate(
-//                "/patient_data/observation_set/*[1]/end_date[text()='2012-12-01T11:00:00.00']", doc, XPathConstants.NODE);
-//        assertNotNull(observationDiagnosisEndDate1);
-        Node observationDiagnosisConceptCD2 = (Node) xpath.evaluate("/patient_data/observation_set/*[2]/concept_cd[text()='185055488771']", doc, XPathConstants.NODE);
-        assertNotNull(observationDiagnosisConceptCD2);
 
-
-        Node observationStartDate2 = (Node) xpath.evaluate(
-                "/patient_data/observation_set/*[2]/start_date[text()='1850-06-12T00:00:00.00']", doc, XPathConstants.NODE);
-        assertNotNull(observationStartDate2);
-//
-//        Node observationEndDate2 = (Node) xpath.evaluate(
-//                "/patient_data/observation_set/*[2]/end_date[text()='2012-12-01T12:00:00.00']", doc, XPathConstants.NODE);
-//        assertNotNull(observationEndDate2);
-//
-        Node observationDiagnosisEventID2 = (Node) xpath.evaluate("/patient_data/observation_set/*[2]/event_id[text()='483572']", doc, XPathConstants.NODE);
-        assertNotNull(observationDiagnosisEventID2);
-//
-//        Node observationDiagnosisObserverCD1 = (Node) xpath.evaluate("/patient_data/observation_set/*[1]/observer_cd", doc, XPathConstants.NODE);
-//        assertNotNull(observationDiagnosisObserverCD1);
-//        assertEquals(observationDiagnosisObserverCD1.getTextContent(), "I.R.C.C.S. CENTRO RIFERIMENTO ONCOLOGICO");
-
-        Node observationObserverCD2 = (Node) xpath.evaluate("/patient_data/observation_set/*[2]/observer_cd", doc, XPathConstants.NODE);
-        assertNotNull(observationObserverCD2);
-        assertEquals(observationObserverCD2.getTextContent(), "I.R.C.C.S. CENTRO RIFERIMENTO ONCOLOGICO");
-
-//
-//        NodeList observers = (NodeList) xpath.evaluate("/patient_data/observer_set/observer", doc, XPathConstants.NODESET);
-//        assertEquals(2, observers.getLength());
-//
-//
-//        Node observerDiagnosisPath1 = (Node) xpath.evaluate(
-//                "/patient_data/observer_set/*[1]/observer_path[text()='Friuli Venezia Giulia']",
-//                doc,
-//                XPathConstants.NODE
-//        );
-//        assertNotNull(observerDiagnosisPath1);
-//
-//        Node observerDiagnosisCD1 = (Node) xpath.evaluate(
-//                "/patient_data/observer_set/*[1]/observer_cd[text()='I.R.C.C.S. CENTRO RIFERIMENTO ONCOLOGICO']",
-//                doc,
-//                XPathConstants.NODE
-//        );
-//        assertNotNull(observerDiagnosisCD1);
-//
-//        Node observerDiagnosisPath2 = (Node) xpath.evaluate(
-//                "/patient_data/observer_set/*[2]/observer_path[text()='Acme\\test']",
-//                doc,
-//                XPathConstants.NODE
-//        );
-//        assertNotNull(observerDiagnosisPath2);
-//
-//        Node observerDiagnosisCD2 = (Node) xpath.evaluate(
-//                "/patient_data/observer_set/*[2]/observer_cd[text()='1832473e-2fe0-452d-abe9-3cdb9879522f']",
-//                doc,
-//                XPathConstants.NODE
-//        );
-//        assertNotNull(observerDiagnosisCD2);
 
 
     }
